b1e20456d20856141d6cdaefc4553b1f6d91f8a6
Ekaji <ekajionah@gmail.com>
2022-09-15 15:06:01 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
+import '../styles/Preloader.css'
+import '../styles/Optin.css'
+import '../styles/Timer.css'
 import '../styles/globals.css'
+import "../styles/style.css";
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
