30db37b268006c08e082c6ae846441549ea674f0
Ekaji <ekajionah@gmail.com>
2022-09-16 22:23:58 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,64 @@ import Optin from "../components/optin/Optin";
 
 const Home: NextPage = ( ) => {
   return (
-    <div className="App bg-slate-600 h-screen">
-      <div className="container">
-        <h1>
-          project 774
-        </h1>
-        Is an initiative by Technology Against Crime in Africa NGO. it is a project initiated to help curb <br />
-        the security challenges and drive the growth of digital Economy in africa through a community based approach. <br />
-        TAc Community Hub is designed to create  an enabling environment for capacity building in<br /> 
-        Digital Specialized Skills and also promote the Central Bank Digital Currency inclusion amongst <br /> 
-        youths in remote locations
-        <h1>
-          <br />
-          Coming Soon
-        </h1>
-        <Timer />
-        {/* <Optin /> */}
-        <Preloader />
-      </div>
+<div className="bg-gray-100 font-sans leading-normal tracking-normal">
+    <div className="container w-full md:max-w-3xl mx-auto pt-20">
+                <h1 className="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">Project 774 - www.remotelocations.io
+                    Stay abreast with the latest developments </h1>
+                <p className="text-sm md:text-base font-normal my-4 text-gray-600">16 SEP 2022 </p>
+      <img src="/logo.png" alt="logo" />
+        <div className="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style={{fontFamily: "Georgia,serif"}}>
+            <div className="font-sans">
+            </div>
+            <div className="py-6 ">
+
+            <p className='font-sans' >
+           
+            It is our vision to take digital transformation to remote locations within Nigeria. <br />
+            The overall goal of PROJECT 774 is to make sure that all the 774 Local Government Areas in Nigeria has at least ONE TECHNOLOGY HUB where youths, students & residents can easily access. 
+            The long term goal of the project is to create over 1,000,000 full-time jobs and 2,500,000 part-time jobs annually as well as digital inclusion of the citizenry in the bottom of the pyramid.
+            Our method is setting up of a technology hub (5,000 sq/meter Land) in the biggest town in each local government. <br />
+            </p>
+            <h3 className='text-3xl mt-6 mb-2 font-sans font-semibold' >Beneficiaries of our infrastructures includes</h3>
+            <ul className='font-sans'>
+                <li className='font-sans'>Students </li>
+                <li className='font-sans'>Enforcement Agencies </li>
+                <li className='font-sans'>Youth coppers </li>
+                <li className='font-sans'>Unemployed citizen </li>
+                <li className='font-sans'>professional bodies willing to explore </li>
+            </ul>
+
+            <h3 className='text-3xl mt-6 mb-2 font-sans  font-semibold' > Available infrastructure for all participants in the hub</h3>
+            <ul>
+                <li className='font-sans'>ICT lab </li>
+                <li className='font-sans'>Library </li>
+                <li className='font-sans'>Sport courts (Tennis, basketball etc) </li>
+                <li className='font-sans'>Vocational training space </li>
+                <li className='font-sans'>Swimming pool </li>
+            </ul>
+
+            <h3 className='text-3xl mt-6 mb-2 font-sans font-semibold' >Full-Time jobs assured for participants after the training</h3>
+            <ul>
+                <li className='font-sans'>IT mentors </li>
+                <li className='font-sans'>Blockchain experts </li>
+                <li className='font-sans'>Drone pilots </li>
+                <li className='font-sans'>Graphics designers </li>
+                <li className='font-sans'>Athletes </li>
+            </ul>
+
+            <h3 className='text-3xl mt-6 mb-2 font-sans font-semibold' >Get involved in this project by Donating:</h3>
+            <ul>
+                <li className='font-sans'>5,000sq/meter land </li>
+                <li className='font-sans'>Laptops and its accessories </li>
+                <li className='font-sans'>Building materials and its accessories </li>
+                <li className='font-sans'>Tennis Court </li>
+                <li className='font-sans'>Swimming pool, et </li>
+            </ul>
+
+        </div>
+              </div>
     </div>
+</div>
   );
 }
 
